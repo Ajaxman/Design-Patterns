f785ac0be0f0efb3369fde4b0d8bf3da92e2cc41
Javier Lopez Lopez <sjavierlopez@gmail.com>
2014-12-13 21:51:55 -0600

    Change for travis CI

--- app.php
+++ app.php
@@ -7,5 +7,5 @@ use Jlopez\DesignPatterns\FactoryPattern\CardEvaluatorFactory;
 $cardEvaluatorFactory = new CardEvaluatorFactory();
 $cardEvaluator = new CardEvaluator($cardEvaluatorFactory);
 $cardEvaluator->setCardNumber(['4916338506082832','5280934283171080','3528000000000000','345936346788903']);
-print_r($cardEvaluator->getCardType());
+
 
